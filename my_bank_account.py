0e20edd09eb5043560e51de03dbc05c4f3ae43d8
хороший человек <yndolokop>
2022-05-06 06:41:48 +0100

    консольный файловый менеджер готов

---
+++ my_bank_account.py
@@ -0,0 +1,24 @@
+history_item = []  # хранит список товаров
+history_price = []  # хранит список цен
+new_balance = 0
+
+
+def shopping_history():  # функция печатает историю покупок из двух списков: названий товара и цен
+    dict_history = dict(zip(history_item, history_price))
+    print('История покупок: ', dict_history)
+
+
+def buy(new_balance):
+    price = int(input('Введите сумму покупки: '))
+    if price > new_balance:
+        print('Недостаточно денег. Пополните счет')
+    elif price <= new_balance:
+        item_name = input('Введите название товара: ')
+        new_balance -= price
+        print('Остаток на счету: ', new_balance)
+        history_item.append(item_name)
+        history_price.append(price)
+        print('Список покупок: ', history_item)
+    else:
+        print('Обратитесь в техподдержку')
+    return new_balance
