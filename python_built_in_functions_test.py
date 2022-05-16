70629984e781462361363d81e4a605e2af253cfa
хороший человек <yndolokop>
2022-05-17 00:59:36 +0100

    консольный файловый менеджер готов

--- python_built_in_functions_test.py
+++ python_built_in_functions_test.py
@@ -1,35 +1,4 @@
-from test_built_in_functions import total, join, clean_list, my_filter, my_map, my_sorted, my_pi, my_pow, my_hypot
-
-
-def test_total_empty() -> None:
-    assert total([]) == 0.0
-
-
-def test_total_single_item() -> None:
-    assert total([110.0]) == 110.0
-
-
-def test_total_many_items() -> None:
-    assert total([1.0, 2.0, 3.0]) == 6.0
-
-
-def test_join_use_case() -> None:
-    assert join([1, 2, 3], ', ') == '1, 2, 3'
-
-
-def test_join_edge_use_case() -> None:
-    assert join([1], ',') == '1'
-
-
-def test_empty_space_delimiter() -> None:
-    assert join([1, 2, 3], '') == '123'
-
-
-def test_clean_list() -> None:
-    assert clean_list(['']) == []
-    assert clean_list([1, 2, 3]) == [1, 2, 3]
-    assert clean_list((1, 1)) == [1, 1]
-    assert clean_list(['']) == list(filter(None, []))
+from test_built_in_functions import my_filter, my_map, my_sorted, my_pi, my_pow, my_hypot
 
 
 def test_my_filter():
--- test_built_in_functions.py
+++ test_built_in_functions.py
@@ -1,25 +1,5 @@
 import math
 
-def total(xs: list[float]) -> float:
-    result: float = 0.0
-    for x in xs:
-        result += x
-    return result
-
-
-def join(xs: list[int], delimiter: str) -> int:
-    result: str = ''
-    for item in xs:
-        if result == '':
-            result = str(item)
-        else:
-            result += delimiter + str(item)
-    return result
-
-
-def clean_list(x):
-    return list(filter(None, x))
-
 
 def my_filter(iterable, function):
     return list(filter(function, iterable))
