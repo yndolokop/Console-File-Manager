a199f449316f78ea9506278c8168e3ce28fd7261
хороший человек <yndolokop>
2022-05-25 04:10:34 +0100

    консольный файловый менеджер готов

--- test_filemanager.py
+++ test_filemanager.py
@@ -20,37 +20,3 @@ def test_files_and_dirs_to_file():
     r = files_and_dirs_to_file()
     if isinstance(r, list):
         assert 'main.py' in r
-
-
-
-
-
-
-
-
-# def test_console_file_manager():
-#     os.mkdir('folder_mk')
-#     assert 'folder_mk' in os.listdir()
-#
-#     original = os.path.join(os.getcwd(), 'vvv.py')
-#     target = os.path.join(os.getcwd(), 'vv.py')
-#     shutil.copy(original, target)
-#     assert shutil.copyfile(original, target)
-#
-#     os.rmdir('folder_mk')
-#     if os.path.exists("vv.py"):
-#         os.remove("vv.py")
-#
-#
-# def test_buy():  # не знаю, можно так делать или нет, но тест проходит если из терминала набрать
-#     assert buy(0, 10) == 0  # 'pytest -s test_filemanager.py'
-#     pyautogui.keyDown("enter")
-#     assert buy(100, 10) > 0
-#     pyautogui.keyDown("enter")
-#     assert buy(100, 10) == 90
-#     assert '' in history_item
-#     assert 10 in history_price
-#
-#
-# def test_get_random_person():
-#     assert len(get_random_person()) == 2
